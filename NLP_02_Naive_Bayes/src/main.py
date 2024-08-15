8613b587a9b633bd0d3cc7ed837a5735d4f33f63
Irina <50323479+irini-git@users.noreply.github.com>
2024-08-15 19:27:06 +0300

    Create main.py

---
+++ NLP_02_Naive_Bayes/src/main.py
@@ -0,0 +1,7 @@
+from sentiment_data import SentimentData
+
+# Initiate class and compute probability
+# by default doing Binary Naive Bayes, as stated BINARY_NAIVES_BAYES = True
+sentiment = SentimentData()
+
+# End -------------------------
